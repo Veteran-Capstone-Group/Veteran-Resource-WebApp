35fbc44db20caad2afffeb51d850deb99bbf0ad8
Timothy Beck <johndaveleet@gmail.com>
2019-10-21 15:29:34 -0600

    added descriptive comments for attributes on the resource table

--- sql/ddlScript.sql
+++ sql/ddlScript.sql
@@ -4,8 +4,13 @@ drop table if exists resource;
 create table resource(
 	resourceTitle varchar(64) not null,
 	resourceDescription varchar(8000) not null,
+	/*resourceCategory will be selected from a drop down list in gui and will be checked if it's one of
+	  a limited selection of categories. Education, Healthcare, food, housing, ect.*/
 	resourceCategory varchar(32) not null,
 	resourceUrl varchar(255) not null,
+	/*resourceImageUrl is an image to be used as a descriptive image or logo for resource for front end use.*/
+	resourceImageUrl varchar(255) not null,
+	/*resourceApprovalStatus is a bit 0 meaning not approved, 1 meaning approved*/
 	resourceApprovalStatus bit not null,
 	index(resourceCategory),
 	primary key(resourceTitle)

9a65877a5fbfcce385e766c4af7ad4085fa930a2
Timothy Beck <johndaveleet@gmail.com>
2019-10-21 16:14:08 -0600

    created opportunity table, finishing DDL script, left comments describing ambiguous keys.

--- sql/ddlScript.sql
+++ sql/ddlScript.sql
@@ -17,5 +17,22 @@ create table resource(
 );
 
 create table opportunity(
-
+	opportunityCompanyName varchar(64) not null,
+	opportunityJobTitle varchar(144) not null,
+	opportunityJobDescription varchar (3000) not null,
+	opportunityJobRequirments varchar(500) not null,
+	/*Job Types are full-time, part-time, temp, intern */
+	opportunityJobType varchar(10) not null,
+	/*Phone numbers must be 11 digit phone numbers*/
+	opportunityContactPhone char(11) not null,
+	opportunityContactEmail varchar(64) not null,
+	opportunityAddress varchar(124) not null,
+	opportunityZipCode char(5) not null,
+	opportunityImageUrl varchar(255) not null,
+	opportunityApprovalStatus BIT not null,
+	/*job type index for filtering jobs by type. */
+	index(opportunityJobType),
+	/*Zipcode indexed for searching jobs by zipcode */
+	index(opportunityZipCode),
+	primary key(opportunityCompanyName)
 );

52e19b87468a6bb16b0f2ce883a5793fd71d8f7a
Timothy Beck <johndaveleet@gmail.com>
2019-10-25 09:15:05 -0600

    added userId getter and setter methods

--- php/Classes/User.php
+++ php/Classes/User.php
@@ -2,7 +2,7 @@
 
 
 namespace VeteranResource\Resource;
-
+use Ramsey\Uuid\Uuid;
 /**
  * Creating Class User for generating new users
  *
@@ -10,6 +10,7 @@ namespace VeteranResource\Resource;
  * @Author Timothy Beck <barricuda1993@yahoo.com>
  */
 class User {
+	use ValidateUuid;
 	/**
 	 * id for this user, this is the primary key
 	 * @var Uuid $userId
@@ -45,6 +46,41 @@ private $userName;
 	 * @var string $userUsername
 	 */
 private $userUsername;
+
+//constructor goes here
+
+//getters and setters
+/**
+ * getter method for user Id
+ *
+ * @return Uuid value of user id
+ */
+public function getUserId(): Uuid {
+	return ($this->userId);
+}
+
+/**
+ * setter method for userId
+ *
+ * @param Uuid $newUserId new value of userId
+ * @throws \TypeError if $newUserId is not a uuid or string
+ */
+public function setUserId($newUserId) {
+	//verify if userId is valid
+	try {
+		$uuid = self::validateUuid($newUserId);
+	} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+		$exceptionType = get_class($exception);
+		throw(new $exceptionType($exception->getMessage(), 0, $exception));
+	}
+	//stores userId
+	$this->userId = $uuid;
+}
+
+
+
+//methods
+
 
 	//JsonSerialize
 }

7b76992ba487acac6deb3baad57f77b5c9ca18c0
Timothy Beck <johndaveleet@gmail.com>
2019-10-25 09:00:26 -0600

    added author Id state variable

---
+++ php/Classes/User.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace VeteranResource\Resource;
+
+/**
+ * Creating Class User for generating new users
+ *
+ * @package VeteranResource\Resource
+ * @Author Timothy Beck <barricuda1993@yahoo.com>
+ */
+class User {
+	/**
+	 * id for this user, this is the primary key
+	 * @var Uuid $userId
+	 */
+private $userId;
+
+	//JsonSerialize
+}

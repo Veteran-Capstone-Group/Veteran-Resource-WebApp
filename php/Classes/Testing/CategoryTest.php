05cbc7b1c010565dd669b52290c7dee97a9bec50
John Johnson-Rodgers <john@johnthe.dev>
2019-10-30 21:27:19 -0600

    finished Unit Test for Category, added phpunit.xml

--- php/Classes/Testing/CategoryTest.php
+++ php/Classes/Testing/CategoryTest.php
@@ -84,5 +84,26 @@ public function testUpdateValidCategory() : void {
 	$this->assertEquals($pdoCategory->getCategoryType(), $this->VALID_CATEGORY_TYPE2);
 }
 
+/**
+ *test creating a category and deleting it
+ */
+public function testDeleteValidTweet(): void {
+	// count the number of rows and save for later
+	$num_rows = $this->getConnection()->getRowCount("category");
+
+	//create a new Category and insert it into mySQL
+	$categoryId = generateUuidV4();
+	$category = new Category($categoryId, $this->VALID_CATEGORY_TYPE);
+	$category->insert($this->getPDO());
+
+	//delete category from mySQL
+	$this->assertEquals($num_rows + 1, $this->getConnection()->getRowCount("category"));
+	$category->delete($this->getPDO());
+
+	//grab data from mySQL and assert the fields match expectations
+	$pdoCategory = Category::getCategoryByCategoryId($this->getPDO(), $category->getCategoryId());
+	$this->assertNull($pdoCategory);
+	$this->assertEquals($num_rows, $this->getConnection()->getRowCount("category"));
+}
 
 }

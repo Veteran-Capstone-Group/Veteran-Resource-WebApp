ac8f1946df524cd8914cbaa8868eb193b7f468ea
John Johnson-Rodgers <john@johnthe.dev>
2019-10-31 15:17:32 -0600

    started Useful class, added boilerplate.

---
+++ php/Classes/Useful.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace VeteranResource\Resource;
+use Ramsey\Uuid\Uuid;
+
+require_once (dirname(__DIR__) . "/vendor/autoload.php");
+
+/**
+ * Creating class Useful, this ia an analog to "likes" that will apply to resources
+ * @package VeteranResource\Resource
+ *
+ * Description: this class will be used to store information on "Usefuls" for the WebApp.
+ *
+ * @author John Johnson-Rodgers
+ */
+

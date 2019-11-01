6be5b697a8baf061045c30fc2c12df81d36718b0
Timothy Beck <johndaveleet@gmail.com>
2019-11-01 10:32:57 -0600

    started UserTest class for unit testing with dockblocks and proper namespacing and requires.)

---
+++ php/Classes/Test/UserTest.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace VeteranResource\Resource;
+use VeteranResource\Resource\User;
+//obtain User Class for testing
+require_once(dirname(__DIR__, 2) . "autoload.php");
+//grab Uuid generator
+require_once(dirname(__DIR__, 2) . "../lib/uuid.php");
+
+/**
+ * PHPUnit test for Class User
+ *
+ * This is a Unit test for the User class. it tests all getters/setters, construct, getFooByBar methods, PDO methods,
+ * and MySQL connection for valid and invalid inputs
+ *
+ * @package VeteranResource\Resource
+ * @see User
+ * @author Timothy Beck <barricuda1993@yahoo.com>
+ */
+class UserTest extends VeteranResourceTest {
+
+}

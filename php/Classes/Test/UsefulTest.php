d31f3395dd52e476fd31df886f49eed9c7ffbc4e
Timothy Beck <johndaveleet@gmail.com>
2019-11-06 14:20:36 -0700

    removed unecessary TODOs and added commenting to startUp() method

--- php/Classes/Test/UsefulTest.php
+++ php/Classes/Test/UsefulTest.php
@@ -2,10 +2,8 @@
 
 
 namespace VeteranResource\Resource\Test;
-use VeteranResource\Resource\Useful;
+use VeteranResource\Resource\{Category, Resource, User, Useful};
 //obtain Class for testing
-require_once(dirname(__DIR__) . "/Test/UserTest.php");
-require_once(dirname(__DIR__) . "/Test/ResourceTest.php");
 require_once(dirname(__DIR__) . "/Test/VeteranResourceTest.php");
 //obtain required dependencies for testing
 require_once(dirname(__DIR__, 2) . "/Classes/autoload.php");
@@ -28,6 +26,11 @@ class UsefulTest extends VeteranResourceTest {
 	public final function setUp(): void {
 		//run the default setUp() method
 		parent::setUp();
+		//create and insert generated category
+		
+		//create and insert generated resource
+
+		//create and insert generated user
 	}
 
 public function testInsertValidUseful() : void {
@@ -35,15 +38,10 @@ public function testInsertValidUseful() : void {
 	$num_rows = $this->getConnection()->getRowCount("useful");
 
 	//Create a new Useful object and insert it into MySQL
-	//TODO change these variables to reference resource and user test variables
-	$usefulResourceId = generateUuidV4();
-	$usefulUserId = generateUuidV4();
-
-	$useful = new Useful($usefulResourceId, $usefulUserId);
+	$useful = new Useful($this->resource->resourceId, $this->user->userId);
 	$useful->insert($this->getPDO());
 
 	//grab data from MySQL and affirm the fields match our query
-	//TODO figure out how to run the insert without get useful by x method
 	$pdoUser = Useful::getUsefulByUsefulResourceId($this->getPDO(), $useful->getUsefulResourceId());
 	$this->assertEquals($num_rows + 1, $this->getConnection()->getRowCount("user"));
 	$this->assertEquals($pdoUser->getUsefulResourceId(), $usefulResourceId);
@@ -66,7 +64,6 @@ public function testInsertValidUseful() : void {
 	 $useful->delete($this->getPDO());
 
 	 //grab data from MySQL and assert that it no longer exists
-	 //TODO figure out how to run the insert without get useful by x method
 	 $pdoUser = Useful::getUsefulByUsefulResourceId($this->getPDO(), $useful->getUsefulResourceId());
 	 $this->assertNull($pdoUser);
 	 $this->assertEquals($num_rows, $this->getConnection()->getRowCount("useful"));

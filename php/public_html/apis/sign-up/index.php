0960a0af3089ee5eae4b84038256b4822eccd6fe
John Johnson-Rodgers <john@johnthe.dev>
2019-11-14 14:26:10 -0700

    started entering required fields for sign up api index.php

--- php/public_html/apis/sign-up/index.php
+++ php/public_html/apis/sign-up/index.php
@@ -15,3 +15,42 @@ use VeteranResource\Resource\User;
  * @author John Johnson-Rodgers <john@johnthe.dev>
  */
 
+//verify session
+if(session_start() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	//connect to mySQL
+	$secrets = new \Secrets("/etc/apache2/capstone-mysql/veteran.ini");
+	$pdo = $secrets->getPdoObject();
+
+	//determine what http method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	if($method === "POST") {
+		//decode the json and turn it into a php object
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		//user Email is a required field
+		if(empty($requestObject->profileAtHandle) === true) {
+			throw(new \InvalidArgumentException ("No profile @handle", 405));
+		}
+		//verify password is there
+
+		//verify that confirm password is there
+
+		//user Name is a required field
+
+		//user Username is a required field
+	}
+
+} catch() {
+
+}

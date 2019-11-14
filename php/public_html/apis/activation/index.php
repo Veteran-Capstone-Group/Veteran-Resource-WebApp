818e469937fe12040b94272dccd2248f293343ee
John Johnson-Rodgers <john@johnthe.dev>
2019-11-14 16:46:40 -0700

    finished activation api

--- php/public_html/apis/activation/index.php
+++ php/public_html/apis/activation/index.php
@@ -41,7 +41,48 @@ try {
 
 	//handle the GET HTTP request
 	if($method === "GET") {
+		//set xsrf token
 		setXsrfCookie();
 
+		//find profile associated with activation token
+		$user = User::getUserByUserActivationToken($pdo, $activation);
+
+		//verify the profile is not null
+		if($user !==null) {
+
+			//make sure the activation token matches
+			if($activation === $user->getUserActivationToken()) {
+
+				//set activation to null
+				$user->setUserActivationToken(null);
+
+				//update the user in the database
+				$user->update($pdo);
+
+				//set the reply for the end user
+				$reply->data = "Thank you for registering your account, you will be automatically redirected to the home page shortly.";
+			}
+		} else{
+			//throw an exception if the token doesn't exist
+			throw(new RuntimeException("Profile with this activation token does not exist."));
+		}
+	} else {
+		//throw an exception if the HTTP request is not a GET
+		throw(new InvalidArgumentException("Invalid HTTP request method."));
 	}
+
+	//update the reply status and message state variables if an exception or type error was thrown
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->data = $exception->getMessage();
+}catch(TypeError $typeError) {
+	$reply->status = $exception->getCode();
+	$reply->status = $exception->getMessage();
 }
+
+//prepare and send the reply
+header("Content-type: application/json");
+if($reply->data === null){
+	unset($reply->data);
+}
+echo json_encode($reply);

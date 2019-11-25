468cac0f925b3862a4ae0b8b3adde7029e8f3102
Timothy Beck <johndaveleet@gmail.com>
2019-11-25 16:00:33 -0700

    started useful.php api and created docbloc

---
+++ php/public_html/apis/useful/useful.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/Classes/autoload.php";
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
+require_once dirname(__DIR__, 3) . "/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/lib/jwt.php";
+require_once dirname(__DIR__, 3) . "/lib/uuid.php";
+
+use VeteranResource\Resource\{User};
+use phpDocumentor\Reflection\Types\Resource_;
+
+/**
+ * api for usefuls
+ *
+ * @author Timothy Beck <Dev@TimothyBeck.com>
+ */
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_status();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	//grab the MySQL connection
+	$secrets = new \Secrets("/etc/apache2/capstone-mysql/veteran.ini");
+	$pdo = $secrets->getPdoObject();
+
+	//determine which HTTP method was used
+	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];

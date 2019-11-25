0c18289bbc30c3f9b4914b90c46d9d3cf0e51bac
Timothy Beck <johndaveleet@gmail.com>
2019-11-25 13:51:46 -0700

    created sanitizing functions for api

--- php/public_html/apis/user/index.php
+++ php/public_html/apis/user/index.php
@@ -29,6 +29,39 @@ try {
 	//grab the MySQL connection
 	$secrets = new \Secrets("/etc/apache2/capstone-mysql/veteran.ini");
 	$pdo = $secrets->getPdoObject();
-	
+
+	//determine which HTTP method was used
+	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
+
+	//sanitize input
+	$userId = filter_input(INPUT_GET, "userId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	//this does stuff
+	$userActivationToken = filter_input(INPUT_GET, "userActivationToken", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$userEmail = filter_input(INPUT_GET, "userEmail", FILTER_SANITIZE_EMAIL, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$userHash = filter_input(INPUT_GET, "userHash", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$userName = filter_input(INPUT_GET, "userName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$userUsername = filter_input(INPUT_GET, "userUsername", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+	if(($method === "DELETE" || $method === "PUT") && (empty($userId) === true)) {
+		throw(new InvalidArgumentException("id caan not be empty", 402));
+	}
+
+	if($method === "GET") {
+		//set xsrf cookie
+		setXsrfCookie();
+
+		if(empty($userUsername) === false) {
+			//get user using userUsername
+			$reply->data = User::getUserByUserUsername($pdo, $userUsername);
+		} elseif(empty($userId) === false) {
+			//get user by user Id
+			$reply->data = User::getUserByUserId($pdo, $userId);
+		} elseif(empty($userActivationToken) === false) {
+			$reply->data = User::getUserByUserActivationToken($pdo, $userActivationToken);
+		}  else {
+			throw (new InvalidArgumentException("Input Required", 400));
+		}
+
+	}
 }
 

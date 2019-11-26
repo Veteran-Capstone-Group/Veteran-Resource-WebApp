ba769c9bd8113f69757063219e08f90cd9df9a21
Timothy Beck <johndaveleet@gmail.com>
2019-11-25 20:00:34 -0700

    finished really short sign-out api

--- php/public_html/apis/sign-out/index.php
+++ php/public_html/apis/sign-out/index.php
@@ -11,7 +11,7 @@ use VeteranResource\Resource\{User, Category, Resource, Useful};
 use phpDocumentor\Reflection\Types\Resource_;
 
 /**
- * api for usefuls
+ * api for signing out
  *
  * @author Timothy Beck <Dev@TimothyBeck.com>
  */
@@ -19,3 +19,29 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_status();
 }
 
+//prepare a null reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	if($method === "GET") {
+		$_SESSION = [];
+		$reply->message = "You are now signed out.";
+	}
+	else {
+		throw (new \InvalidArgumentException("Invalid HTTP method request"));
+	} //determine what kind of error was thrown
+} catch(\Exception | \TypeError $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+}
+
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+//echo reply to front end
+echo json_encode($reply);

fe08234e24afc5483479c5c1514a9ed1ce277318
Timothy Beck <johndaveleet@gmail.com>
2019-11-25 17:35:51 -0700

    created sanitizing for sign-in api

--- php/public_html/apis/sign-in/index.php
+++ php/public_html/apis/sign-in/index.php
@@ -28,3 +28,52 @@ try {
 	//grab the MySQL connection
 	$secrets = new \Secrets("/etc/apache2/capstone-mysql/veteran.ini");
 	$pdo = $secrets->getPdoObject();
+
+	//determine which HTTP method is being used.
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//if method is POST handle sign-in
+	if($method === "POST") {
+		//make sure the XSRF Token is valid
+		verifyXsrf();
+		//process the request content and decode the json object into a php object
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+		//check to make sure the password and username field is not empty.s
+		if(empty($requestObject->userUsername) === true) {
+			throw(new \InvalidArgumentException("Please provide your username", 401));
+		} else {
+			$userEmail = filter_var($requestObject->userUsername, FILTER_SANITIZE_STRING);
+		}
+
+		if(empty($requestObject->userPassword) === true) {
+			throw(new \InvalidArgumentException("Please enter a password", 401));
+		} else {
+			$userPassword = $requestObject->userPassword;
+		}
+
+		//grab the profile from the database with the email
+		$user = User::getUserByUserUsername($pdo, $userUsername);
+		if(empty($user) === true) {
+			throw(new InvalidArgumentException("Invalid Username", 401));
+		}
+		$user->setUserActivationToken('null');
+		$user->update($pdo);
+
+		//verify hash is correct
+		if(password_verify($requestObject->userPassword, $user->getUserHash()) === false) {
+			throw(new \InvalidArgumentException("Password or Username is incorrect.", 401));
+		}
+
+		//grab user from database and put into session
+		$user = User::getUserByUserId($pdo, $user->getUserId());
+
+		$_SESSION["user"] = $user;
+
+		//create authentincation payload
+
+
+
+
+	}
+}

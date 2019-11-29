caef657c7d4a58609297c398d268871ebd5d7cab
John Johnson-Rodgers <john@johnthe.dev>
2019-11-29 13:12:18 -0700

    Created base page for sign up, began SignUpForm

---
+++ app/src/pages/sign-up/SignUp.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {SignUpForm} from "./SignUpForm";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+export const SignUp = () => {
+	return (
+		<main className="d-block align-items-center">
+			<Container fluid="false">
+				<Row>
+					<Col>
+						<SignUpForm/>
+					</Col>
+				</Row>
+			</Container>
+		</main>
+	);
+};
+
---
+++ app/src/pages/sign-up/SignUpForm.js
@@ -0,0 +1,4 @@
+import React, {useState} from 'react';
+import {httpConfig} from "../../shared/utils/http-config";
+import * as Yup from "yup";
+import {Formik} from "formik";

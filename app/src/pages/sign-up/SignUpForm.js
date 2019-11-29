8fbf7f21f12ebf40e9d45ee481d2c65dc77d4eac
John Johnson-Rodgers <john@johnthe.dev>
2019-11-29 13:50:29 -0700

    finished SignUpForm.js

--- app/src/pages/sign-up/SignUpForm.js
+++ app/src/pages/sign-up/SignUpForm.js
@@ -2,3 +2,54 @@ import React, {useState} from 'react';
 import {httpConfig} from "../../shared/utils/http-config";
 import * as Yup from "yup";
 import {Formik} from "formik";
+
+//define 'state variables' to be used in sign up form
+export const SignUpForm = () => {
+	const signUp = {
+		userEmail: "",
+		userPassword: "",
+		userPasswordConfirm: "",
+		userName: "",
+		userUsername: ""
+	};
+	//set status and setStatus to null initial state
+	const [status, setStatus] = useState(null);
+
+	// initiate yup validator
+	const validator = Yup.object().shape({
+		userEmail: Yup.string()
+			.email("Email Address must be valid.")
+			.required("Email Address is required."),
+		userPassword: Yup.string()
+			.required("Password is required.")
+			.min(8, "Password must be at least eight characters!"),
+		userPasswordConfirm: Yup.string()
+			.required("Password is required.")
+			.min(8, "Password must be at least eight characters!"),
+		userName: Yup.string()
+			.required("Name is required."),
+		userUsername: Yup.string()
+			.required("Username is required.")
+	});
+	const submitSignUp = (values, {resetForm}) => {
+		httpConfig.post("/apis/sign-up/", values)
+			.then(reply => {
+					let {message, type} = reply;
+					setStatus({message, type});
+					if(reply.status === 200) {
+						resetForm();
+					}
+				}
+			);
+	}
+
+	return (
+		<Formik
+			initialValues={signUp}
+			onSubmit={submitSignUp}
+			validationSchema={validator}
+			>
+			{SignUpFormContent}
+		</Formik>
+	)
+};
--- app/src/pages/sign-up/SignUpModal.js
+++ app/src/pages/sign-up/SignUpModal.js
@@ -5,7 +5,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-export const SignUp = () => {
+export const SignUpModal = () => {
 	return (
 		<main className="d-block align-items-center">
 			<Container fluid="false">

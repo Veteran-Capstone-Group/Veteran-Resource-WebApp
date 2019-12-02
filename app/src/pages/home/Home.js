fdf6969f15738f1cdf1c512bc381fbcccec2ac90
Timothy Beck <johndaveleet@gmail.com>
2019-12-02 14:20:57 -0700

    building this homepage and it's scareh. DAMN REACT YOU SCAREH

--- app/src/pages/home/Home.js
+++ app/src/pages/home/Home.js
@@ -1,9 +1,31 @@
-import React from "react"
+import React from "react";
+import {useSelector, useDispatch} from "react-redux";
+import {getAllCategories} from "../../shared/actions/category";
 
 export const Home = () => {
+	const categories = useSelector(state => state.categories);
+	const dispatch = useDispatch();
+
+	const effects = () => {
+		dispatch(getAllCategories());
+	};
+
+	const inputs = [];
+
+	useEffect(effects,inputs);
+
 	return (
 		<>
-			<h1>Home</h1>
+			<h1>Home Test</h1>
+			{categories.map(category => {
+				return(
+					<Div id={{homepageCategory}} key={category.categoryId}>
+						<a id={`${category.categoryType}-link`} href={'test.com'}> /**/
+							<img id={`${category.categoryType}-image`} src={`../../../../documentation/svg-icons/${category.categoryType}.svg`} alt={category.categoryType} />
+						</a>
+					</Div>
+				)
+			})}
 		</>
 	)
 };

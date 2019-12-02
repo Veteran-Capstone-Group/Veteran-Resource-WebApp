d3ac2f8c8b94f356dec9bba0fe975f1ff2ed7802
John Johnson-Rodgers <john@johnthe.dev>
2019-12-02 11:50:41 -0700

    added index.js

---
+++ app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css';
+
+const App = () => ( <h1 className="text-info">hello world</h1> );
+ReactDOM.render(<App/>, document.querySelector('#root'));

c2909c0db41ff26c3311e9635d5bcf2111a774f5
John Johnson-Rodgers <john@johnthe.dev>
2019-12-02 11:47:46 -0700

    readded package-lock to .gitignore

--- app/src/pages/four-oh-four/FourOhFour.js
+++
@@ -1,3 +0,0 @@
-import React from "react"
-
-export const FourOhFour = () => <h1>Y U NO FIND</h1>;
--- app/src/pages/home/Home.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export const Home = () => {
-	return (
-		<>
-			<h1>Home</h1>
-		</>
-	)
-};

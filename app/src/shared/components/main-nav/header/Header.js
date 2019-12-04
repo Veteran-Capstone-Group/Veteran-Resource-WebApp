915e34d4aae6a9059062927511f5c95a2aabd921
Timothy Beck <johndaveleet@gmail.com>
2019-12-04 11:20:41 -0700

    added desktop header, it still needs styling

--- app/src/shared/components/main-nav/header/Header.js
+++ app/src/shared/components/main-nav/header/Header.js
@@ -3,10 +3,27 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 export const Header = () => {
 	const isMobile = window.innerWidth < 480;
-	if(isMobile){//create mobile header
+	if(isMobile) {//create mobile header
 
-	}else{//create desktop header
-
+	} else {//create desktop header
+		return (
+			<>
+				<Navbar collapseOnSelect expand="lg">
+					<Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+					<Navbar.Collapse id="responsive-navbar-nav">
+						<Nav className="mr-auto">
+							<NavDropdown title="Dropdown" id="collasible-nav-dropdown" border-right border-light>
+								<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+								<NavDropdown.Divider/>
+								<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+							</NavDropdown>
+						</Nav>
+					</Navbar.Collapse>
+					<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+				</Navbar>
+			</>
+		)
 	}
-
 };

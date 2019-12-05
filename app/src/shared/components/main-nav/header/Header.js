4f1452859c73e68d58bc9eb3c3d6605e2563fddd
Timothy Beck <johndaveleet@gmail.com>
2019-12-05 14:33:54 -0700

    stylied the dropdown menu, repositioned it and resized it and modified padding and margin. Next I need to modify and stylize the text

--- app/src/shared/components/main-nav/header/Header.js
+++ app/src/shared/components/main-nav/header/Header.js
@@ -17,12 +17,15 @@ export const Header = () => {
 					<Navbar.Toggle aria-controls="responsive-navbar-nav"/>
 					<Navbar.Collapse id="responsive-navbar-nav">
 						<Col xs={2} className={`border border-dark`}>
-								<NavDropdown title="Dropdown" id="collasible-nav-dropdown" className={`text-dark`}>
-									<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-									<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-									<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-									<NavDropdown.Divider/>
-									<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+								<NavDropdown title="$CategoryTitle" id="collasible-nav-dropdown" className={`text-dark`}>
+									<NavDropdown.Item href="#action/3.1">Clothing/Food</NavDropdown.Item><NavDropdown.Divider/>
+									<NavDropdown.Item href="#action/3.2">Disability</NavDropdown.Item><NavDropdown.Divider/>
+									<NavDropdown.Item href="#action/3.3">Education</NavDropdown.Item><NavDropdown.Divider/>
+									<NavDropdown.Item href="#action/3.4">Employment</NavDropdown.Item><NavDropdown.Divider/>
+									<NavDropdown.Item href="#action/3.5">Mental Health</NavDropdown.Item><NavDropdown.Divider/>
+									<NavDropdown.Item href="#action/3.6">Healthcare</NavDropdown.Item><NavDropdown.Divider/>
+									<NavDropdown.Item href="#action/3.7">Housing</NavDropdown.Item><NavDropdown.Divider/>
+									<NavDropdown.Item id={`bottomMenuItem`} href="#action/3.8">Miscellaneous</NavDropdown.Item>
 								</NavDropdown>
 						</Col>
 						<Col xs={8}>

aef12972e14a64c5adf6ecc215ff4d58f586c870
Timothy Beck <johndaveleet@gmail.com>
2019-12-06 11:32:57 -0700

    started signinform with some boilerplate.

---
+++ app/src/shared/components/main-nav/sign-in/SignInForm.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import {SignInFormContent} from "./SignInFormContent";
+import {httpConfig} from "../../../utils/http-config";
+import * as Yup from "yup";
+import {Formik} from "formik";
+
+export const SignInForm = () => {
+
+	const signIn = {
+		userUsername = "";
+		userPassword = ""
+	};
+
+};

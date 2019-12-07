e2b30e0fe40b76ec2b826abb3c4997773ae0ebcb
Timothy Beck <johndaveleet@gmail.com>
2019-12-07 16:20:26 -0700

    finished formik signin form

--- app/src/shared/components/main-nav/sign-in/SignInForm.js
+++ app/src/shared/components/main-nav/sign-in/SignInForm.js
@@ -4,6 +4,14 @@ import {httpConfig} from "../../../utils/http-config";
 import * as Yup from "yup";
 import {Formik} from "formik";
 
+/**
+ * signInForm validation and API Logic
+ *
+ * @returns {*} formik Sign In Form
+ * @author Timothy Beck <Dev@TimothyBeck.com>
+ */
+
+//define signin state variable to utilize in sign-in form
 export const SignInForm = () => {
 
 	const signIn = {
@@ -19,6 +27,20 @@ export const SignInForm = () => {
 			.required("Password is required.")
 	});
 	const submitSignIn = (values, {resetForm, setStatus}) => {
+		httpConfig.post("/apis/sign-in/", values)
+			.then(reply => {
+				let {message, type} = reply;
+				setStatus({message, type});
+				if(reply.status === 200) {
+					resetForm();
+					setStatus({message, type});
+				}
+			})
+	};
 
-	}
+	return (
+		<Formik onSubmit={submitSignIn} initialValues={signIn} validationSchema={validator}>
+			{SignInFormContent}
+		</Formik>
+	)
 };

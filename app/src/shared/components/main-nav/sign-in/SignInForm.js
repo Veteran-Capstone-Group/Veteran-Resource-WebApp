6305cc1c231abfac3b368c9356b25d080697d737
Timothy Beck <johndaveleet@gmail.com>
2019-12-07 16:09:49 -0700

    started signinform with yup validator

--- app/src/shared/components/main-nav/sign-in/SignInForm.js
+++ app/src/shared/components/main-nav/sign-in/SignInForm.js
@@ -7,8 +7,18 @@ import {Formik} from "formik";
 export const SignInForm = () => {
 
 	const signIn = {
-		userUsername = "";
-		userPassword = ""
+		userUsername: "",
+		userPassword: ""
 	};
 
+	//initiate yup validator
+	const validator = Yup.object().shape({
+		userUsername: Yup.string()
+			.required("Username is required."),
+		userPassword: Yup.string()
+			.required("Password is required.")
+	});
+	const submitSignIn = (values, {resetForm, setStatus}) => {
+
+	}
 };

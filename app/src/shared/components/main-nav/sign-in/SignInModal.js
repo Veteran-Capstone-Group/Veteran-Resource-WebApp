352e4fbf77b2ff66b58efdbaf390a2642960f2dc
Timothy Beck <johndaveleet@gmail.com>
2019-12-07 16:26:27 -0700

    created JSXsign in modal with logic to handle hiding and showing form

---
+++ app/src/shared/components/main-nav/sign-in/SignInModal.js
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import {SignInForm} from "./SignInForm";
+;
+
+//create signInModal JSX to serve SignInForm
+export const SignInModal = () => {
+	//initializes form in hidden state
+	const [show, setShow] = useState(false);
+	//function hides modal
+	const handleClose = () => setShow(false);
+	//function shows modal
+
+	return (
+		<>
+			<Button variant="primary" className={"m-4"} size='lg' onClick={handleShow}>
+				Sign In
+			</Button>
+
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>Sign In</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<SignInForm/>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose}>
+						Close
+					</Button>
+					<Button variant="primary" onClick={handleClose}>
+						Sign In
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</>
+	);
+};

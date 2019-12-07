6e45bca7e8ba21214b2611dda982c3864a31c95b
Timothy Beck <johndaveleet@gmail.com>
2019-12-07 16:27:46 -0700

    removed extre semicolon

--- app/src/shared/components/main-nav/sign-in/SignInModal.js
+++ app/src/shared/components/main-nav/sign-in/SignInModal.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import {SignInForm} from "./SignInForm";
-;
+
 
 //create signInModal JSX to serve SignInForm
 export const SignInModal = () => {

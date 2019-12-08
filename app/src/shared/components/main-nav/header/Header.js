c7846f376683d5127082c326dfe211b12c3acd47
John Johnson-Rodgers <john@johnthe.dev>
2019-12-08 16:46:25 -0700

    changed links in navmenu

--- app/src/shared/components/main-nav/header/Header.js
+++ app/src/shared/components/main-nav/header/Header.js
@@ -17,23 +17,33 @@ export const Header = () => {
 					<Navbar.Toggle aria-controls="navbar-nav"/>
 					<Navbar.Collapse id="navbar-nav">
 						<Col id="navDropdownButton">
-							<NavDropdown title="$CategoryTitle" id="collapsible-nav-dropdown" className={`mobile-fix text-dark`}>
+							<NavDropdown title="$CategoryTitle" id="collapsible-nav-dropdown"
+											 className={`mobile-fix text-dark`}>
 								<Col xs={10}>
-									<NavDropdown.Item href="#action/3.1">Clothing/Food</NavDropdown.Item><NavDropdown.Divider/>
-									<NavDropdown.Item href="#action/3.2">Disability</NavDropdown.Item><NavDropdown.Divider/>
-									<NavDropdown.Item href="#action/3.3">Education</NavDropdown.Item><NavDropdown.Divider/>
-									<NavDropdown.Item href="#action/3.4">Employment</NavDropdown.Item><NavDropdown.Divider/>
-									<NavDropdown.Item href="#action/3.5">Mental Health</NavDropdown.Item><NavDropdown.Divider/>
-									<NavDropdown.Item href="#action/3.6">Healthcare</NavDropdown.Item><NavDropdown.Divider/>
-									<NavDropdown.Item href="#action/3.7">Housing</NavDropdown.Item><NavDropdown.Divider/>
-									<NavDropdown.Item id={`bottomMenuItem`} href="#action/3.8">Miscellaneous</NavDropdown.Item>
+									<NavDropdown.Item
+										href="/Category/501c7665-a4b1-47ab-a157-13d198f67d97">Clothing/Food</NavDropdown.Item><NavDropdown.Divider/>
+									<NavDropdown.Item
+										href="/Category/b20fe0cd-43e4-4878-9d29-a4ecb57678a3">Disability</NavDropdown.Item><NavDropdown.Divider/>
+									<NavDropdown.Item
+										href="/Category/b2b19ae1-7c88-4f5d-baa2-b2ebf964cd2a">Education</NavDropdown.Item><NavDropdown.Divider/>
+									<NavDropdown.Item
+										href="/Category/faef9afc-61e2-4238-a634-b15164ebdbae">Employment</NavDropdown.Item><NavDropdown.Divider/>
+									<NavDropdown.Item href="/Category/0535ca67-9c12-4cc9-9450-e2faa89b91db">Mental
+										Health</NavDropdown.Item><NavDropdown.Divider/>
+									<NavDropdown.Item
+										href="/Category/34b09b0c-08a9-46a5-829b-0e5b7f385f5a">Healthcare</NavDropdown.Item><NavDropdown.Divider/>
+									<NavDropdown.Item
+										href="/Category/8167caec-0d53-47c7-8a86-9b226a325eae">Housing</NavDropdown.Item><NavDropdown.Divider/>
+									<NavDropdown.Item id={`bottomMenuItem`}
+															href="/Category/a48077fe-3955-460d-9bb8-e04e48aad125">Miscellaneous</NavDropdown.Item>
 								</Col>
 								<Col xs={2}>
 									<span></span>
 								</Col>
 							</NavDropdown>
 						</Col>
-							<img src={logo} className={'img-fluid d-inline'} id={"logoimage"} alt={"ABQ Veterans Logo"}/>
+						<a href="/"><img src={logo} className={'img-fluid d-inline'} id={"logoimage"}
+											  alt={"ABQ Veterans Logo"}/></a>
 					</Navbar.Collapse>
 				</Navbar>
 			</>
@@ -45,22 +55,32 @@ export const Header = () => {
 					<Navbar.Toggle aria-controls="responsive-navbar-nav"/>
 					<Navbar.Collapse id="responsive-navbar-nav">
 						<Col xs={2} id="navDropdownButton">
-								<NavDropdown title="$CategoryTitle" id="collapsible-nav-dropdown" className={`text-dark`}>
-									<NavDropdown.Item href="#action/3.1">Clothing/Food</NavDropdown.Item><NavDropdown.Divider/>
-									<NavDropdown.Item href="#action/3.2">Disability</NavDropdown.Item><NavDropdown.Divider/>
-									<NavDropdown.Item href="#action/3.3">Education</NavDropdown.Item><NavDropdown.Divider/>
-									<NavDropdown.Item href="#action/3.4">Employment</NavDropdown.Item><NavDropdown.Divider/>
-									<NavDropdown.Item href="#action/3.5">Mental Health</NavDropdown.Item><NavDropdown.Divider/>
-									<NavDropdown.Item href="#action/3.6">Healthcare</NavDropdown.Item><NavDropdown.Divider/>
-									<NavDropdown.Item href="#action/3.7">Housing</NavDropdown.Item><NavDropdown.Divider/>
-									<NavDropdown.Item id={`bottomMenuItem`} href="#action/3.8">Miscellaneous</NavDropdown.Item>
-								</NavDropdown>
+							<NavDropdown title="$CategoryTitle" id="collapsible-nav-dropdown" className={`text-dark`}>
+								<NavDropdown.Item
+									href="/Category/501c7665-a4b1-47ab-a157-13d198f67d97">Clothing/Food</NavDropdown.Item><NavDropdown.Divider/>
+								<NavDropdown.Item
+									href="/Category/b20fe0cd-43e4-4878-9d29-a4ecb57678a3">Disability</NavDropdown.Item><NavDropdown.Divider/>
+								<NavDropdown.Item
+									href="/Category/b2b19ae1-7c88-4f5d-baa2-b2ebf964cd2a">Education</NavDropdown.Item><NavDropdown.Divider/>
+								<NavDropdown.Item
+									href="/Category/faef9afc-61e2-4238-a634-b15164ebdbae">Employment</NavDropdown.Item><NavDropdown.Divider/>
+								<NavDropdown.Item href="/Category/0535ca67-9c12-4cc9-9450-e2faa89b91db">Mental
+									Health</NavDropdown.Item><NavDropdown.Divider/>
+								<NavDropdown.Item
+									href="/Category/34b09b0c-08a9-46a5-829b-0e5b7f385f5a">Healthcare</NavDropdown.Item><NavDropdown.Divider/>
+								<NavDropdown.Item
+									href="/Category/8167caec-0d53-47c7-8a86-9b226a325eae">Housing</NavDropdown.Item><NavDropdown.Divider/>
+								<NavDropdown.Item id={`bottomMenuItem`}
+														href="/Category/a48077fe-3955-460d-9bb8-e04e48aad125">Miscellaneous</NavDropdown.Item>
+							</NavDropdown>
 						</Col>
 						<Col xs={8}>
-							<Navbar.Brand className={"col-12 text-center font-weight-bold text-dark"} id="DesktopHeader" href="#home">ABQ Veterans</Navbar.Brand>
+							<Navbar.Brand className={"col-12 text-center font-weight-bold text-dark"} id="DesktopHeader"
+											  href="#home">ABQ Veterans</Navbar.Brand>
 						</Col>
 						<Col xs={2}>
-							<span></span>
+							<a href="/"><img src={logo} className={'img-fluid d-inline'} id={"logoimage"}
+												  alt={"ABQ Veterans Logo"}/></a>
 						</Col>
 					</Navbar.Collapse>
 				</Navbar>

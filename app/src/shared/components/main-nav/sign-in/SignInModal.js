30e2a1779ba9ab0749e904802c3478ea2124b461
Timothy Beck <johndaveleet@gmail.com>
2019-12-08 15:55:59 -0700

    removed extram buttons from signup/signin modals

--- app/src/shared/components/main-nav/sign-in/SignInModal.js
+++ app/src/shared/components/main-nav/sign-in/SignInModal.js
@@ -32,14 +32,6 @@ export const SignInModal = () => {
 				<Modal.Body>
 					<SignInForm/>
 				</Modal.Body>
-				<Modal.Footer>
-					<Button variant="secondary" onClick={handleClose}>
-						Close
-					</Button>
-					<Button variant="primary" onClick={handleClose}>
-						Sign In
-					</Button>
-				</Modal.Footer>
 			</Modal>
 		</>
 	);

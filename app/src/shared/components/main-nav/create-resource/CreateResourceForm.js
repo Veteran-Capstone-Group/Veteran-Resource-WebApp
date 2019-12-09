3154a92bb4dd9cdf68aea03864d63be47fdc0b09
Timothy Beck <johndaveleet@gmail.com>
2019-12-08 17:06:19 -0700

    created docblock and yup validator for createresourceform

---
+++ app/src/shared/components/main-nav/create-resource/CreateResourceForm.js
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+import {CreateResourceFormConten} from "./CreateResourceFormContent";
+import {httpConfig} from "../../../utils/http-config";
+import * as Yup from "yup";
+import {Formik} from "formik";
+
+/**
+ * CreateResourceFormConten validation and API Logic
+ *
+ * @returns {*} formik create resource Form
+ * @author Timothy Beck <Dev@TimothyBeck.com>
+ */
+
+//define create resource state variable to utilize in create resource form
+export const CreateResourceForm = () => {
+
+	const createResource = {
+		resourceCategoryId: "",
+		resourceTitle: "",
+		resourceOrganization: "",
+		resourceDescription: "",
+		resourceAddress: "",
+		resourcePhone: "",
+		resourceEmail: "",
+		resourceUrl: "",
+		resourceImageUrl: ""
+	};
+
+	//initiate yup validator
+	const validator = Yup.object().shape({
+		resourceCategoryId: Yup.string()
+			.required("Category Type is required."),
+		resourceTitle: Yup.string()
+			.required("")
+			.max(64, "Title can not be longer than 64 characters."),
+		resourceOrganization: Yup.string()
+			.max(124, "Organization name can not be longer than 124 characters."),
+		resourceDescription: Yup.string()
+			.required("")
+			.max(300, "Description can not be longer than 300 characters."),
+		resourceAddress: Yup.string()
+			.max(124, "Address can not be longer than 124 characters."),
+		resourcePhone: Yup.string()
+			.max(20, "Phone number too long"),
+		resourceEmail: Yup.string()
+			.max(124, "Email can not be longer than 124 characters."),
+		resourceUrl: Yup.string()
+			.required("")
+			.max(255, "URL can not be longer than 255 characters."),
+		resourceImageUrl: Yup.string()
+			.max(255, "URL can not be longer than 255 characters.")
+	})
+};

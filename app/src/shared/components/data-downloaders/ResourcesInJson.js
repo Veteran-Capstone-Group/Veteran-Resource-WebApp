1a55da3d9f1b30da4b65ded3e02edc73893beaa2
John Johnson-Rodgers <john@johnthe.dev>
2020-01-05 09:40:39 -0700

    both buttons work, altered formatting

--- app/src/shared/components/data-downloaders/ResourcesInJson.js
+++ app/src/shared/components/data-downloaders/ResourcesInJson.js
@@ -10,16 +10,27 @@ import _ from 'lodash';
 
 export const ResourcesInJson = ({match}) => {
 
-	const [copySuccess, setCopySuccess] = useState('');
-	const textAreaRef = useRef(null);
+	const [jsonCopySuccess, setJsonCopySuccess] = useState('');
+	const [csvCopySuccess, setCsvCopySuccess] = useState('');
+	const jsonAreaRef = useRef(null);
+	const csvAreaRef = useRef(null);
 
-	function copyToClipboard(e) {
-		textAreaRef.current.select();
+	function jsonToClipboard(e) {
+		jsonAreaRef.current.select();
 		document.execCommand('copy');
 		// This is just personal preference.
 		// I prefer to not show the the whole text area selected.
 		e.target.focus();
-		setCopySuccess('Copied!');
+		setJsonCopySuccess('Copied!');
+	};
+
+	function csvToClipboard(e) {
+		csvAreaRef.current.select();
+		document.execCommand('copy');
+		// This is just personal preference.
+		// I prefer to not show the the whole text area selected.
+		e.target.focus();
+		setCsvCopySuccess('Copied!');
 	};
 
 	const resources = useSelector(state => (state.resource ? state.resource : []));
@@ -27,20 +38,14 @@ export const ResourcesInJson = ({match}) => {
 	//assigns useDispatch to dispatch variable
 	const dispatch = useDispatch();
 
-	// Define the side effects that will occur in the application, e.g., code that handles dispatches to redux, API requests, or timers.
-	// The dispatch function takes actions as arguments to make changes to the store/redux.
-
-	//variable function
-
-
-
 	//pass side effects with inputs to useEffect
 	useEffect(() => {
 		dispatch(getAllResources());});
 
 	let results = "";
 	let csvObject = "";
-	let csvResult="resourceId, resourceCategoryId, resourceUserId, resourceAddress, resourceApprovalStatus, resourceDescription, resourceEmail, resourceImageUrl, resourceOrganization, resourcePhone, resourceTitle, resourceUrl";
+	let csvResult = "";
+	let csvTitle="resourceId, resourceCategoryId, resourceUserId, resourceAddress, resourceApprovalStatus, resourceDescription, resourceEmail, resourceImageUrl, resourceOrganization, resourcePhone, resourceTitle, resourceUrl";
 	let jsonResources = resources.map((resourceItem, index) => {
 		if (index !==0) {
 			results = ", "
@@ -49,11 +54,11 @@ export const ResourcesInJson = ({match}) => {
 		return results;
 	});
 	let csvResources = resources.map((resourceItem) => {
-		csvObject = <li key={resourceItem.resourceId} className="list-group-item">{resourceItem.resourceId + ", " + resourceItem.resourceCategoryId + ", " + resourceItem.resourceUserId
+		csvObject = resourceItem.resourceId + ", " + resourceItem.resourceCategoryId + ", " + resourceItem.resourceUserId
 			+ ", " + resourceItem.resourceAddress + ", " + resourceItem.resourceApprovalStatus + ", " +
 			resourceItem.resourceDescription + ", " + resourceItem.resourceEmail + ", " + resourceItem.resourceImageUrl +
 			", " + resourceItem.resourceOrganization + ", " + resourceItem.resourcePhone + ", " + resourceItem.resourceTitle
-			+ ", " + resourceItem.resourceUrl}</li>;
+			+ ", " + resourceItem.resourceUrl;
 		csvResult = csvObject;
 		return csvResult;
 	});
@@ -63,31 +68,38 @@ export const ResourcesInJson = ({match}) => {
 			<div id="mainContent">
 				<Container fluid="true">
 					<Row>
-						<Col xs="6" style={{maxHeight: "500px"}}>
+						<Col lg="6" style={{height: "500px"}}>
 							<Card className="h-100 overflow-auto">
 								<h5 className="card-title">
 									Json Resources
 								</h5>
-								<textarea ref={textAreaRef} className="card-text" value={"["+jsonResources+"]"}/>
+								<textarea ref={jsonAreaRef} className="card-text h-75" value={"["+jsonResources+"]"}/>
 								{
 									/* Logical shortcut for only displaying the
 										button if the copy command exists */
 									document.queryCommandSupported('copy') &&
-									<div className="pt-3">
-										<button onClick={copyToClipboard}>Copy</button>
-										{copySuccess}
+									<div className="py-3">
+										<button onClick={jsonToClipboard}>Copy</button> <br/>
+										{jsonCopySuccess}
 									</div>
 								}
 							</Card>
 						</Col>
-						<Col xs="6" style={{maxHeight: "500px"}}>
+						<Col lg="6" style={{maxHeight: "1000px"}}>
 							<Card className="h-100 overflow-auto">
 								<h5 className="card-title">
 									CSV Resources
 								</h5>
-								<p className="card-text list-group">
-									{csvResources}
-								</p>
+								<textarea ref={csvAreaRef} className="card-text h-75" value={"["+csvTitle+"\n"+csvResources.join("\n")+"]"}/>
+								{
+									/* Logical shortcut for only displaying the
+										button if the copy command exists */
+									document.queryCommandSupported('copy') &&
+									<div className="py-3">
+										<button onClick={csvToClipboard}>Copy</button> <br/>
+										{csvCopySuccess}
+									</div>
+								}
 							</Card>
 						</Col>
 					</Row>
